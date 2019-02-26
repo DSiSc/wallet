0bee677fd87fe31df265026147ba384c1576dbe6
caohaitao <2310852380@qq.com>
2019-02-26 10:30:47 +0800

    fix bug accounts_test

--- accounts/errors_test.go
+++ accounts/errors_test.go
@@ -1,13 +1,12 @@
 package accounts
 
 import (
-	"github.com/ethereum/go-ethereum/accounts"
 	"testing"
 )
 
 
 func TestAuthNeededError(t *testing.T) {
-	ErrLocked  := accounts.NewAuthNeededError("password or unlock")
+	ErrLocked  := NewAuthNeededError("password or unlock")
 	ErrLocked.Error()
 }
 
