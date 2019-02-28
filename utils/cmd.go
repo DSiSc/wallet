2c20c5db7ba32f91da89f4e5107c296e32a0755f
caohaitao <2310852380@qq.com>
2019-02-28 10:58:50 +0800

    [new function] SendTransaction

--- utils/cmd.go
+++ utils/cmd.go
@@ -1,13 +1,26 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
+	cmn "github.com/DSiSc/apigateway/common"
+	"github.com/DSiSc/apigateway/rpc/lib/types"
+	"github.com/DSiSc/craft/log"
+	"github.com/DSiSc/craft/types"
 	"github.com/DSiSc/wallet/accounts"
 	"github.com/DSiSc/wallet/accounts/keystore"
 	"io/ioutil"
-	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
+
+
+
+
+func statusOK(code int) bool { return code >= 200 && code <= 299 }
 
 func AccountConfig(keystoreDir string) (int, int, string, error) {
 	scryptN := keystore.StandardScryptN
@@ -60,7 +73,7 @@ func GetUnlockedKey(address string, passphrase string) (accounts.Account, *keyst
 func GetUnlockedKeyByDir(address string, passphrase string, keystoreDir string) (accounts.Account, *keystore.Key, error){
 	scryptN, scryptP, keydir, err := AccountConfig(keystoreDir)
 	if err != nil {
-		log.Printf("Failed to read configuration: %v", err)
+		fmt.Printf("Failed to read configuration: %v", err)
 		return accounts.Account{}, nil, err
 	}
 
@@ -69,11 +82,48 @@ func GetUnlockedKeyByDir(address string, passphrase string, keystoreDir string) 
 	//get account by stirng address
 	account, err := MakeAddress(ks, address)
 	if err != nil {
-		log.Printf("Failed to make stirng address to account: %v", err)
+		fmt.Printf("Failed to make stirng address to account: %v", err)
 		return accounts.Account{}, nil, err
 	}
 
 	ac, key, err := ks.GetDecryptedKey(account, passphrase)
 
 	return ac, key, err
+}
+
+//Send a signed transaction
+func SendTransaction(tx *types.Transaction) (string, error) {
+	//construct payload
+	from := fmt.Sprintf("0x%x", *(tx.Data.From))
+	to := from
+	gas := "0x" + strconv.FormatInt(int64(tx.Data.GasLimit),16)
+	gasprice := "0x" + tx.Data.Price.String()
+	value := "0x" + tx.Data.Amount.String()
+	nonce := "0x" + strconv.FormatInt(int64(tx.Data.AccountNonce),16)
+	data := "" + string(tx.Data.Payload)
+	payload := fmt.Sprintf(`{"jsonrpc": "2.0", "method": "eth_sendTransaction", "id": "0", "params": [{"from": "%s", "to": "%s","gas": "%s","gasPrice": "%s", "value": "%s","nonce": "%s","data": "%s"}]}`,
+		from, to, gas, gasprice, value, nonce, data)
+
+	//new and send request
+	req, _ := http.NewRequest("POST", "http://127.0.0.1:47768/", strings.NewReader(payload))
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Error("http send request failed: %v", err)
+		return "", err
+	}
+
+	//resolve response
+	blob, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error("read response msg has failed: %v", err)
+		return "", err
+	}
+	recv := new(rpctypes.RPCResponse)
+	json.Unmarshal(blob, recv)
+
+	var result cmn.Hash
+	json.Unmarshal(recv.Result, &result)
+	txHash := fmt.Sprintf("0x%x\n", result)
+	return txHash, nil
 }
--- utils/cmd_test.go
+++ utils/cmd_test.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"fmt"
+	"github.com/DSiSc/wallet/common"
+	local "github.com/DSiSc/wallet/core/types"
 	"github.com/cespare/cp"
 	"io/ioutil"
+	"math/big"
 	"path/filepath"
 	"testing"
 )
@@ -31,4 +34,24 @@ func TestGetUnlockedKey(t *testing.T) {
 	ks := filepath.Join(datadir, "keystore")
 	ac, key, err := GetUnlockedKeyByDir("94cdad6a9c62e418608f8ef5814821e74db3e331", "", ks)
 	fmt.Print(ac, key, err)
+}
+
+func TestSendTransaction(t *testing.T) {
+	nonce := uint64(1)
+	from := common.Address{
+		0xb2, 0x6f, 0x2b, 0x34, 0x2a, 0xab, 0x24, 0xbc, 0xf6, 0x3e,
+		0xa2, 0x18, 0xc6, 0xa9, 0x27, 0x4d, 0x30, 0xab, 0x9a, 0x15,
+	}
+	to := from
+	amount := big.NewInt(0)
+	gaslimit := uint64(0)
+	gasprice := big.NewInt(1000)
+	// data := nil
+	tx := local.NewTransaction(nonce, to, amount, gaslimit, gasprice, nil, from)
+	txHash, err := SendTransaction(tx)
+	if err != nil {
+		fmt.Println("send tx has failed, ", err)
+		return
+	}
+	fmt.Println(txHash)
 }
