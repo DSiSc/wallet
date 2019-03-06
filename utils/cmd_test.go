e6875d56f7fc282104497256a09c2c0af5e2bf13
caohaitao <2310852380@qq.com>
2019-03-06 16:59:41 +0800

    [add unit test]utils test

--- utils/cmd_test.go
+++ utils/cmd_test.go
@@ -1,9 +1,13 @@
 package utils
 
 import (
+	"fmt"
+	"github.com/DSiSc/wallet/common"
+	local "github.com/DSiSc/wallet/core/types"
 	"github.com/cespare/cp"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"math/big"
 	"path/filepath"
 	"testing"
 )
@@ -44,26 +48,26 @@ func TestGetUnlockedKey(t *testing.T) {
 	assert.Equal(t, nil, err)
 }
 
-//func TestSendTransaction(t *testing.T) {
-//	nonce := uint64(1)
-//	from := common.Address{
-//		0xb2, 0x6f, 0x2b, 0x34, 0x2a, 0xab, 0x24, 0xbc, 0xf6, 0x3e,
-//		0xa2, 0x18, 0xc6, 0xa9, 0x27, 0x4d, 0x30, 0xab, 0x9a, 0x15,
-//	}
-//	to := from
-//	amount := big.NewInt(0)
-//	gaslimit := uint64(0)
-//	gasprice := big.NewInt(1000)
-//	// data := nil
-//	tx := local.NewTransaction(nonce, to, amount, gaslimit, gasprice, nil, from)
-//	txHash, err := SendTransaction(tx)
-//	if err != nil {
-//		fmt.Println("send tx has failed, ", err)
-//		return
-//	}
-//	fmt.Println(txHash)
-//}
-//
+func TestSendTransaction(t *testing.T) {
+	nonce := uint64(1)
+	from := common.Address{
+		0xb2, 0x6f, 0x2b, 0x34, 0x2a, 0xab, 0x24, 0xbc, 0xf6, 0x3e,
+		0xa2, 0x18, 0xc6, 0xa9, 0x27, 0x4d, 0x30, 0xab, 0x9a, 0x15,
+	}
+	to := from
+	amount := big.NewInt(0)
+	gaslimit := uint64(0)
+	gasprice := big.NewInt(1000)
+	// data := nil
+	tx := local.NewTransaction(nonce, to, amount, gaslimit, gasprice, nil, from)
+	txHash, err := SendTransaction(tx)
+	if err != nil {
+		fmt.Println("send tx has failed, ", err)
+		return
+	}
+	fmt.Println(txHash)
+}
+
 //func TestSendRawTransaction(t *testing.T) {
 //	nonce := uint64(1)
 //	from := common.Address{
